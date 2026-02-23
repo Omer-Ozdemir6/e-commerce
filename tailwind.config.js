b43a82e8f2d96b459bd90dbfff6d79320cfa7266
Ömer Özdemir <byozdemir06@gmail.com>
2026-02-23 22:05:53 +0300

    tüm bagımlılıklar yüklendi

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Bu satırın doğruluğundan emin ol!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
