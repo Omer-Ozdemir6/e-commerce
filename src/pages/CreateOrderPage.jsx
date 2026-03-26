917d3ea191022754e9a895c262bcd9cc4ffd429b
Ömer Özdemir <byozdemir06@gmail.com>
2026-03-26 14:03:05 +0300

    update

--- src/pages/CreateOrderPage.jsx
+++ src/pages/CreateOrderPage.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { fetchAddresses, deleteAddress } from '../store/action/addressActions';
 import { fetchCards, deleteCard } from '../store/action/paymentActions';
 import { createOrder } from '../store/action/orderActions';
 import AddressForm from '../components/AddressForm';
 import CardForm from '../components/CardForm';
 import { Plus, Trash2, Edit2, User, Phone, CreditCard, ChevronRight, Check } from 'lucide-react';
+import { deleteAddress, fetchAddresses } from '../store/action/addressActions';
 
 export default function CreateOrderPage() {
     const dispatch = useDispatch();
